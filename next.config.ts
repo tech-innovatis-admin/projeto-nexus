d8e913f4d5e8ec6abcc830e9db242d1216e37c9c
Gabriel Vitor <gabrielvitormchaves@gmail.com>
2026-01-29 15:59:42 -0300

    fix: ajuste final

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "i.pravatar.cc", pathname: "/**" },
+    ],
+  },
+};
+
+export default nextConfig;

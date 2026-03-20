f75a914595507402824828a46443fef15f11e354
innova-samuel <samuel.araujo@innovatismc.com>
2026-03-20 09:17:58 -0300

    fix/ocultar navbar na tela de login

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -9,8 +9,8 @@ export default function AppShell({ children }: { children: React.ReactNode }) {
   const [isOpen, setIsOpen] = useState(false);
   const pathname = usePathname();
 
-  // NÃO deve aparecer o menu
-  const hideLayout = pathname === "/login";
+  // NÃO deve aparecer nas rotas:
+  const hideLayout = pathname === "/login" || pathname === "/";
 
   if (hideLayout) {
     return <>{children}</>;
